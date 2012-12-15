61f8b195ad5a22f0720a6f4b01c8e40a330f5c60
Stanley Jones <stanley@diligentcreative.com>
2012-12-14 16:28:29 -0800

    Added support for local configuration, put local-config.php in .gitignore.

--- wp-config.php
+++ wp-config.php
@@ -14,24 +14,34 @@
  * @package WordPress
  */
 
+
 // ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
 
-/** MySQL database username */
-define('DB_USER', 'username_here');
+if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
 
-/** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+	define( 'WP_LOCAL_DEV', true );
+	include( dirname( __FILE__ ) . '/local-config.php' );
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+} else {
 
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
+	/** The name of the database for WordPress */
+	define('DB_NAME', 'database_name_here');
+	
+	/** MySQL database username */
+	define('DB_USER', 'username_here');
+	
+	/** MySQL database password */
+	define('DB_PASSWORD', 'password_here');
+	
+	/** MySQL hostname */
+	define('DB_HOST', 'localhost');
+}
+	
+	/** Database Charset to use in creating database tables. */
+	define('DB_CHARSET', 'utf8');
+	
+	/** The Database Collate type. Don't change this if in doubt. */
+	define('DB_COLLATE', '');
 
 /**
  * Custom Content Directory.
@@ -40,6 +50,9 @@ define('DB_COLLATE', '');
  */
 define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
 define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
+
+define('WP_HOME', 'http://' . $_SERVER['SERVER_NAME']);
+define('WP_SITEURL', 'http://' . $_SERVER['SERVER_NAME'] . '/wp');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -50,14 +63,14 @@ define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
+define('AUTH_KEY',         '4JD[j4M~>:Hd2r0faZ#.VT|wf;A-Foq5Q$>T<W>%X#C}GBC27-z/^_NPS^N9&liG');
+define('SECURE_AUTH_KEY',  'C&v[<z&;$V8`^G`jn~Db_xI%jvL.33](6^X6&+<p`kH-?;DO.8+DHOJO=o[?/`o9');
+define('LOGGED_IN_KEY',    'e|e84*#!Ws_p_~+Tb=H_W];UQx%&@V ,SIeK+5C`TZLM$q^+6Xg)I5@quk&9WR>@');
+define('NONCE_KEY',        'H/Ng>Ta6M<FjR[;v|0+filW04sEN<zg[B7j.R~0@aU&VKk]Ml%+EAJF;0VF)Ef?^');
+define('AUTH_SALT',        'dvo,LMchl4_;8SC&muY| Oyc,*j2e3bmum3u[hvuj==uI513`upB>&7-+|+kS-w+');
+define('SECURE_AUTH_SALT', 'Y2a!l|4n!+6.gfDe-rbJ5^.h^KwdaN]M25gWd3r}wM9x|R|clhQR:A3hP:vn%Yf`');
+define('LOGGED_IN_SALT',   ' `56qxFE !_pVJthN h:K-VJ1kL;i#$-+$EA_N>,7jX3-~-tL:5Lb[v3+FA Ean_');
+define('NONCE_SALT',       'k4 REb36zMdOy4|o>vKF7~G2VX-imiYOP9*4+[([I*!1#7(a-;gWz/e*L#pc{>U2');
 
 /**#@-*/
 
