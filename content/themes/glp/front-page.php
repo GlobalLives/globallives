d52658906bb5141bf18db6ed2a95051bc3e66a94
Stanley Jones <stanley@diligentcreative.com>
2013-01-28 12:17:20 -0800

    Tightened UI on homepage.

--- content/themes/glp/front-page.php
+++ content/themes/glp/front-page.php
@@ -1,7 +1,7 @@
 <div id="stage">
 	<?php while (have_posts()) : the_post(); ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class('front-page'); ?><?php if (has_post_thumbnail()) : ?> data-bg="<?php echo wp_get_attachment_url( get_post_thumbnail_id($post->ID) ); ?>"<?php endif; ?>>
-		<div class="page-excerpt"><?php the_excerpt(); ?></div>		
+<!-- 		<div class="page-excerpt"><?php the_excerpt(); ?></div> -->
 		<div class="page-content"><?php the_content(); ?></div>
 		<h3><?php _e('Explore the Collection','glp'); ?></h3>
 		<p><a class="btn" href="<?php echo get_permalink(get_page_by_title( 'Explore the Collection' )); ?>"><i class="icon-globe"></i> Map View</a></p>
--- content/themes/glp/templates/nav-featured.php
+++ content/themes/glp/templates/nav-featured.php
@@ -1,9 +1,9 @@
 <?php $participants = get_posts(array( 'post_type' => 'participant' )); ?>
 <nav id="nav-featured">
 	<div class="nav-featured-inner container">
-		<h2><?php _e('Featured Videos','glp'); ?> (<?php echo 1 + count($participants); ?>)</h2>
+<!-- 		<h2><?php _e('Featured Videos','glp'); ?> (<?php echo 1 + count($participants); ?>)</h2> -->
 		<div id="featured-carousel" class="carousel slide row">
-		    <ul class="carousel-inner">
+		    <ul class="carousel-inner row">
 		    	<li class="featured-thumbnail span2 active"><a href="<?php home_url(); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/logo-featured.png"></a></li>
 		    	<?php foreach ($participants as $participant) : ?>
 		    	<li class="featured-thumbnail participant-thumbnail span2" data-id="<?php echo $participant->ID; ?>"><img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id($participant->ID,'thumbnail') ); ?>"></li>
