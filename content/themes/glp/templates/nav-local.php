46c174fcc83bdc73af9e064e60ed29b02a542a46
Stanley Jones <stanley@diligentcreative.com>
2013-02-05 11:34:21 -0800

    Fixed local nav for sub-sub-pages. Works as expected. Fixes #65.

--- content/themes/glp/templates/nav-local.php
+++ content/themes/glp/templates/nav-local.php
@@ -3,7 +3,7 @@
 	$ancestors = get_post_ancestors( $post->ID );
 	if ( $ancestors ) {
 	    # Page is a sub-page, return all pages under its top level ancestor
-	    wp_list_pages('depth=0&title_li=&child_of='.$ancestors[0]);
+	    wp_list_pages('depth=0&title_li=&child_of='.end($ancestors));
 	} else {
 	    # Page is top level, return all of its descendants
 	    wp_list_pages('depth=0&title_li=&child_of='.$post->ID);
