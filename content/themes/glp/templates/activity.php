ceff0a91da8346983111566162a1c0d9919e930c
Stanley Jones <stanley@diligentcreative.com>
2013-02-07 12:25:22 -0800

    Replaced remaining Gravatar stragglers. Fixes #51.

--- content/themes/glp/templates/activity.php
+++ content/themes/glp/templates/activity.php
@@ -1,7 +1,7 @@
 <?php global $activity; //$activity_user = get_userdata($activity['activity_user']); ?>
 <!-- <?php var_dump($activity); ?> -->
 	<li class="activity <?php echo $activity['activity_type']; ?> row">
-		<div class="activity-thumbnail span2"><?php echo get_avatar($activity['activity_user']); ?></div>
+		<div class="activity-thumbnail span2"><img src="<?php the_profile_thumbnail_url($activity['activity_user'],'medium'); ?>"></div>
 		<div class="activity-meta">
 			<span class="activity-username">@<?php echo $activity_user->user_login; ?></span> 
 			<?php echo $activity['activity_description']; ?> 
