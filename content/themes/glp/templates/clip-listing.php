12c63bedc90e4419d83dcc8010d941c463bc4a2f
Stanley Jones <stanley@diligentcreative.com>
2013-02-11 23:51:32 -0800

    Changed meta info for clips. Closes #141.

--- content/themes/glp/templates/clip-listing.php
+++ content/themes/glp/templates/clip-listing.php
@@ -4,7 +4,8 @@
     	<div class="clip-thumbnail span2" data-clip-id="<?php echo $clip->ID; ?>"><img src="http://img.youtube.com/vi/<?php the_field('youtube_id', $clip->ID); ?>/0.jpg"></div>
     	<div class="span2">
     		<h5 class="clip-title"><?php echo $clip->post_title; ?></h5>
-    		<p class="clip-duration"><?php the_field('duration',$clip->ID); ?></p>
+    		<p class="clip-starttime"><?php _e('Start Time','glp'); ?>: <?php the_field('start_time',$clip->ID); ?></p>
+    		<p class="clip-duration"><?php _e('Duration','glp'); ?>: <?php the_field('duration',$clip->ID); ?></p>
     		<?php if ($download_url = get_field('download_url',$clip->ID)) : ?><a class="" href="<?php echo $download_url; ?>"><i class="icon icon-white icon-arrow-down"></i> Download</a><?php endif; ?>
     		<!-- <a class="" href=""><i class="icon icon-white icon-plus"></i> Queue</a> -->
     	</div>
