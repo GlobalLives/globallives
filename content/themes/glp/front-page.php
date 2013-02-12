9321b07e5041f7924880d3cc9faf3e939c9e654a
Stanley Jones <stanley@diligentcreative.com>
2013-02-12 00:01:20 -0800

    Homepage tweaks. Fixes #147.

--- content/themes/glp/front-page.php
+++ content/themes/glp/front-page.php
@@ -2,7 +2,7 @@
 	<?php while (have_posts()) : the_post(); ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class('front-page'); ?>>
 		<div class="page-content"><?php the_content(); ?></div>
-		<h3><?php _e('Explore the Collection','glp'); ?></h3>
+		<h4><?php _e('Explore the Collection','glp'); ?></h4>
 		<p><a class="btn" href="<?php echo get_permalink(get_page_by_title( 'Explore the Collection' )); ?>"><i class="icon-globe"></i> Map View</a></p>
 	</article>
 	<?php endwhile; ?>
