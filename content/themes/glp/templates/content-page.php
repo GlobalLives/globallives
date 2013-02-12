2b70d4196cc8d5abae930d0d347c62c590242239
Stanley Jones <stanley@diligentcreative.com>
2013-02-12 13:25:10 -0800

    Static page tweaks. Fixes #130.

--- content/themes/glp/templates/content-page.php
+++ content/themes/glp/templates/content-page.php
@@ -9,7 +9,7 @@
 <?php while (have_posts()) : the_post(); ?>
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header>
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <h2 class="page-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-summary">
