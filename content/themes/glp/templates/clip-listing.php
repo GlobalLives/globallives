ed148c77638ec7206ae305670209048ccdfc1b4c
Stanley Jones <stanley@diligentcreative.com>
2013-02-11 23:35:17 -0800

    Removed queue buttons from clips. Fixes #151.

--- content/themes/glp/templates/clip-listing.php
+++ content/themes/glp/templates/clip-listing.php
@@ -5,7 +5,8 @@
     	<div class="span2">
     		<h5 class="clip-title"><?php echo $clip->post_title; ?></h5>
     		<p class="clip-duration"><?php the_field('duration',$clip->ID); ?></p>
-    		<?php if ($download_url = get_field('download_url',$clip->ID)) : ?><a class="" href="<?php echo $download_url; ?>"><i class="icon icon-white icon-arrow-down"></i> Download</a><?php endif; ?> <a class="" href=""><i class="icon icon-white icon-plus"></i> Queue</a>
+    		<?php if ($download_url = get_field('download_url',$clip->ID)) : ?><a class="" href="<?php echo $download_url; ?>"><i class="icon icon-white icon-arrow-down"></i> Download</a><?php endif; ?>
+    		<!-- <a class="" href=""><i class="icon icon-white icon-plus"></i> Queue</a> -->
     	</div>
     </div>
 </article>
