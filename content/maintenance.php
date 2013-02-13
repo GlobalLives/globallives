8ddcdab43e852f41e122efe1637262bb3d268381
Stanley Jones <stanley@diligentcreative.com>
2013-02-12 22:45:38 -0800

    Added maintenance mode temp page.

---
+++ content/maintenance.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>	   <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>		   <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>		   <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>GlobalLives.org</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width">
+
+		<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+		<link rel="stylesheet" href="../placeholder/css/normalize.css">
+		<link rel="stylesheet" href="../placeholder/css/style.css">
+		<script src="../placeholder/js/vendor/modernizr-2.6.2.min.js"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
+
+		<div id="page">
+		
+		<!-- Add your site or application content here -->
+		<header id="masthead" class="clearfix">
+			<div class="container">
+			<h1 class="site-title"><a href="/">Global Lives Project</a></h1>
+			<nav class="site-navigation"><ul>
+				<li><a href="#about">About</a></li>
+				<li><a href="#fund">Fund</a></li>
+				<li><a href="#credits">Credits</a></li>
+			</ul></nav>
+			<nav class="social-media-links"><ul>
+				<li><a href="https://twitter.com/globallives" class="icon-tw">Twitter</a></li>
+				<li><a href="https://www.facebook.com/globallives" class="icon-fb">Facebook</a></li>
+				<li><a href="http://www.youtube.com/globallives" class="icon-yt">YouTube</a></li>
+			</ul></nav>
+			</div>
+		</header>
+		
+		<section id="main">
+			<div class="container">
+				<h1 id="site-description">A Video Library of<br>Life Experience</h1>
+				<a id="play-button">&#9658; Play intro video</a>
+				<iframe id="kickstarter-video" width="640" height="480" src="http://www.kickstarter.com/projects/896398266/globally-collaborative-filmmaking-lives-in-transit/widget/video.html" frameborder="0"> </iframe>
+				<p class="caption">Photo: Global Lives Producer Ya-Hsuan Huang records the life of Edith Kaphuka, Ngwale Village, Zomba District, Malawi.</p>
+			</div>
+		</section>
+
+		<section id="about">
+			<div class="container">
+				<h1>About the Global Lives Project</h1>
+				<h2>The Global Lives Project is a video library of life experience. A nonprofit, volunteer-driven effort of filmmakers and translators, we create and curate films that capture 24 continuous hours in the life of individuals from around the world. We use visual media to cultivate the ethics of global citizenship and cross-cultural empathy for a global audience.</h2>
+			</div>
+		</section>
+
+		<section id="fund">
+			<div class="container">
+				<h1>Help Kickstart <cite>Lives in Transit!</cite></h1>
+				<iframe frameborder="0" height="380" src="http://www.kickstarter.com/projects/896398266/globally-collaborative-filmmaking-lives-in-transit/widget/card.html" width="220"></iframe>
+				<p>We are beginning production on <cite>Lives in Transit</cite>, a series capturing 240 hours in the lives of 10 individuals who move people and goods throughout our world. From bus fare collectors to flight attendants, mule packers to bicycle rickshaw drivers, <cite>Lives in Transit</cite> will bring together nearly 100 accomplished filmmakers and media artists to undertake a globally collaborative work.</p>
+			</div>
+		</section>				
+
+		<section id="credits">
+			<div class="container">
+				<img src="../placeholder/img/nea.png" alt="National Endowment for the Arts logo"> <img src="../placeholder/img/mthd.png" alt="Method logo">
+				<h1>Major Construction Happening Here</h1>
+				<p>Thanks to generous support from the <a href="http://arts.gov/">National Endowment for the Arts</a> and <a href="http://method.com/">Method</a>, we’ve got a whole new website in the works! Our new video-based web platform will engage visitors with the full Global Lives collection in unprecedented ways. Much more than an archival project, the web platform will enable visitors to interact with and enrich the content, generating thematic tags, translations, commentaries, and even new video submissions.</p>
+				<p>Allowing visitors to become participants in the Global Lives Project, this advanced web interface is vital to transforming Global Lives’ exhibit design into an interactive video installation. The new website will launch in early 2013.</p>
+				<p><a class="linky" href="http://globallives.org/en/">View our old site.</a></p>
+			</div>
+		</section>
+		
+		<footer>
+			<div class="container">
+				<p>The Global Lives Project is a 501(c)3 non-profit organization in the United States of America. Tax ID #61-1524216.</p>
+				<p>The Global Lives Project work is provided under Creative Commons licenses, please see <a href="http://globallives.org/en/license/">licensing</a> for more details.</p>
+		</footer>
+		
+		</div><!-- #page -->
+		
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.2.min.js"><\/script>')</script>
+		<script src="../placeholder/js/plugins.js"></script>
+		<script src="../placeholder/js/main.js"></script>
+
+		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+	</body>
+</html>
