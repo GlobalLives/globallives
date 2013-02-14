b8e8f0b93c231c4e664a7b2cdc0dd875c9475b6f
Stanley Jones <stanley@diligentcreative.com>
2013-02-14 15:37:56 -0800

    Removed explore buttons from home. Closes #177.

--- content/themes/glp/front-page.php
+++ content/themes/glp/front-page.php
@@ -2,8 +2,6 @@
 	<?php while (have_posts()) : the_post(); ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class('front-page'); ?>>
 		<div class="page-content"><?php the_content(); ?></div>
-		<h4><?php _e('Explore the Collection','glp'); ?></h4>
-		<p><a class="btn" href="<?php echo get_permalink(get_page_by_title( 'Explore the Collection' )); ?>"><i class="icon-globe"></i> Map View</a></p>
 	</article>
 	<?php endwhile; ?>
 </div>
