fd20f84bc6f895316d73fe13fd33a6674b29ee67
Stanley Jones <stanley@diligentcreative.com>
2013-02-15 15:39:55 -0800

    Changes to clip-loading interaction. Closes #186.

--- content/themes/glp/templates/clip-listing.php
+++ content/themes/glp/templates/clip-listing.php
@@ -2,7 +2,7 @@
 <article class="participant-clip-listing">
     <div class="row">
     	<div class="clip-thumbnail span2" data-clip-id="<?php echo $clip->ID; ?>"><img src="http://img.youtube.com/vi/<?php the_field('youtube_id', $clip->ID); ?>/0.jpg"></div>
-    	<div class="span2">
+    	<div class="span4">
     		<h5 class="clip-title"><?php echo $clip->post_title; ?></h5>
     		<p class="clip-duration"><?php the_field('duration',$clip->ID); ?></p>
     		<?php if ($download_url = get_field('download_url',$clip->ID)) : ?><a class="" href="<?php echo $download_url; ?>"><i class="icon icon-white icon-arrow-down"></i> Download</a><?php endif; ?>
