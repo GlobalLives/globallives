28e94667d9a67c11bc9b72fd5bb0babb55f7a96e
Stanley Jones <stanley@diligentcreative.com>
2013-03-26 13:39:06 -0700

    Map and grid view.

--- content/themes/glp/page-explore.php
+++ content/themes/glp/page-explore.php
@@ -1,11 +1,32 @@
-<div id="mapview"></div>
+<?php $participants = get_posts(array( 'post_type' => 'participant', 'posts_per_page' => -1 )); ?>
+<?php get_template_part('templates/nav','explore'); ?>
+
+<!-- Grid View -->
+<div id="gridview" class="view">
+	<?php foreach ($participants as $participant) : ?>
+		<div class="participant-grid"><a href="<?php echo get_permalink($participant->ID); ?>">
+			<div class="participant-meta">
+				<h3><?php echo $participant->post_title; ?></h3>
+				<p><?php the_field('location',$participant->ID); ?></p>
+			</div>
+			<img src="<?php the_participant_thumbnail_url( $participant->ID ); ?>">
+		</a></div>
+	<?php endforeach; ?>
+</div>
+
+<!-- Map View -->
+<div id="mapview" class="view"></div>
 <div class="overlay"></div>
+
 <div id="popover" class="span4">
 	<h3><span class="popover-name"></span> &mdash; <span class="popover-location"></h3>
 	<div class="row">
 		<div class="span1"><img class="popover-thumbnail" src=""></div>
 		<div class="span3">
 			<b><?php _e('Occupation'); ?>:</b> <span class="popover-occupation"></span><br>
+			<b><?php _e('Series'); ?>:</b> <span class="popover-series"></span>
+				
+			</p>
 			<p><a class="btn popover-permalink" href="">&#9658;&nbsp;<?php _e('Full Story','glp'); ?></a></p>
 		</div>
 	</div>
@@ -14,8 +35,17 @@
 
 <script>
 var participants = [
-<?php $participants = get_posts(array( 'post_type' => 'participant', 'posts_per_page' => -1 )); foreach ($participants as $participant) : ?>
-	{ name: '<?php echo $participant->post_title; ?>', permalink: '<?php echo get_permalink($participant->ID); ?>', thumbnail: '<?php the_participant_thumbnail_url( $participant->ID ); ?>', occupation: '<?php the_field('occupation', $participant->ID); ?>', dob: '<?php the_field('dob', $participant->ID); ?>', location: '<?php the_field('location', $participant->ID); ?>', continent: '<?php the_field('continent', $participant->ID); ?>', latitude: <?php the_field('latitude', $participant->ID); ?>, longitude: <?php the_field('longitude', $participant->ID); ?> },
+<?php foreach ($participants as $participant) : ?>
+	{ name: '<?php echo $participant->post_title; ?>', 
+	permalink: '<?php echo get_permalink($participant->ID); ?>', 
+	series: '<?php echo get_the_term_list($participant->ID,'series'); ?>', 
+	thumbnail: '<?php the_participant_thumbnail_url( $participant->ID ); ?>', 
+	occupation: '<?php the_field('occupation', $participant->ID); ?>', 
+	dob: '<?php the_field('dob', $participant->ID); ?>', 
+	location: '<?php the_field('location', $participant->ID); ?>', 
+	continent: '<?php the_field('continent', $participant->ID); ?>', 
+	latitude: <?php the_field('latitude', $participant->ID); ?>, 
+	longitude: <?php the_field('longitude', $participant->ID); ?> },
 <?php endforeach; ?>
 ];
 </script>
---
+++ content/themes/glp/templates/nav-explore.php
@@ -0,0 +1,44 @@
+<nav id="nav-explore" class="navbar">
+	<div class="nav-explore-inner navbar-inner container">
+		<ul class="nav">
+			<li>
+				<a class="btn btn-gridview active"><i class="icon icon-th-large icon-white"></i></a>
+				<a class="btn btn-mapview"><i class="icon icon-globe icon-white"></i></a>
+			</li>
+			<li>Series
+				<select>
+					<option>All</option>
+					<option>The First Ten</option>
+				</select>
+			</li>
+			<li>Gender 
+				<select>
+					<option>All</option>
+					<option>Male</option>
+					<option>Female</option>
+				</select>
+			</li>
+			<li>Income
+				<select>
+					<option>All</option>
+					<option>&lt;$1/day</option>
+					<option>$1&ndash;5/day</option>
+					<option>$5&ndash;10/day</option>
+					<option>$10&ndash;20/day</option>
+					<option>$20+/day</option>
+				</select>
+			</li>
+			<li>Age
+				<select>
+					<option>All</option>
+					<option>0&ndash;10</option>
+					<option>10&ndash;25</option>
+					<option>25&ndash;40</option>
+					<option>40&ndash;65</option>
+					<option>65+</option>
+				</select>
+			</li>
+			<li><input type="checkbox" /> Show Proposed</li>
+		</ul>
+	</div>
+</nav>
