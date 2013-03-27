54ec41e609b43cea48940d862a13d5b6f731059e
Stanley Jones <stanley@diligentcreative.com>
2013-03-27 13:31:07 -0700

    Style improvements on Series page.

--- content/themes/glp/templates/nav-series.php
+++ content/themes/glp/templates/nav-series.php
@@ -4,7 +4,7 @@
 		<div id="series-carousel" class="carousel slide">
 			<ul class="carousel-inner">
 				<div class="item active row">
-					<li class="series-thumbnail home-thumbnail span2 active"><h4><?php echo $series->name; ?></h4><?php echo count($participants); ?> <?php _e('Lives'); ?></li>
+					<li class="series-thumbnail home-thumbnail span2 active"><h4><?php echo $series->name; ?></h4><?php echo $series->count; ?> <?php _e('Lives'); ?></li>
 					<?php $participants = get_posts(array( 'post_type' => 'participant', 'posts_per_page' => 5, 'series' => $series->slug )); // First row only grabs 5, because of the "Home" thumbnail ?>
 					<?php foreach ($participants as $participant) : ?>
 					<li class="series-thumbnail participant-thumbnail span2" data-id="<?php echo $participant->ID; ?>"><img src="<?php the_participant_thumbnail_url( $participant->ID, 'small' ); ?>"></li>
