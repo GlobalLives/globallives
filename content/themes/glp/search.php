3e89b564d86946e3b41936974fa7f505c7a61ca3
Stanley Jones <stanley@diligentcreative.com>
2013-05-28 15:31:05 -0700

    Updated searching.

--- content/themes/glp/search.php
+++ content/themes/glp/search.php
@@ -1,4 +1,3 @@
-<?php query_posts( array_merge(array( 'posts_per_page' => -1 ), $wp_query->query )); // Show all results ?>
 <h1 class="search-title section-title"><?php echo __('Search','glp'); ?></h1>
 
 <div class="search-results-container page-container container">
@@ -8,26 +7,27 @@
 			<div class="search-sidebar">
 				<h4><?php _e('Narrow your search','glp'); ?></h4>
 				<hr>
-										
 				<h4><?php _e('Content type','glp'); ?></h4>
 				<label class="checkbox"><input type="checkbox" name="post_type[]" checked value="page" />Pages</label>
-				<label class="checkbox"><input type="checkbox" name="post_type[]" checked value="post" />Articles</label>
+				<label class="checkbox"><input type="checkbox" name="post_type[]" checked value="post" />Posts</label>
 				<label class="checkbox"><input type="checkbox" name="post_type[]" checked value="participant" />Participants</label>
 				<label class="checkbox"><input type="checkbox" name="post_type[]" checked value="tribe_events" />Events</label>
-
-<!--
-					<h4><?php _e('Theme','glp'); ?></h4>
-					<h4><?php _e('Time Period','glp'); ?></h4>
-					<h4><?php _e('Region','glp'); ?></h4>
--->
-				
 			</div>
 		</div>
 		
 		<div class="span9">
 			<div class="search-entries">
 				<h3><span class="results-found"><?php global $wp_query; $total_results = $wp_query->found_posts; echo $total_results; ?></span> <?php _e('results with','glp'); ?> '<?php the_search_query(); ?>'</h3> 
+				
+				<?php if (!have_posts()) : ?>
+				<div class="alert alert-block fade in">
+					<p><?php _e('Sorry, no results were found.', 'glp'); ?></p>
+				</div>
+				<?php endif; ?>
+
+				<?php while (have_posts()) : the_post(); ?>
 				<?php get_template_part('templates/result', get_post_type()); ?>
+				<?php endwhile; ?>
 			</div>
 		</div>
 	</div>
