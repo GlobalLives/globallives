fd9a04e8328fbe886ac14f9b0a8aaa9030787648
Stanley Jones <stanley@diligentcreative.com>
2013-05-28 11:17:24 -0700

    Tightened up Explore buttons, switched header font to Montreal Medium.

--- content/themes/glp/front-page.php
+++ content/themes/glp/front-page.php
@@ -4,12 +4,11 @@
 		<div class="page-content"><?php the_content(); ?></div>
 	</article>
 	<?php endwhile; ?>
+	<div id="explore">
+		<h4><?php _e('Explore the Collection','glp'); ?></h4>
+		<a href="/explore/#gridview" class="btn btn-inverse"><i class="icon icon-th-large"></i> <?php _e('Grid View','glp'); ?></a>
+		<a href="/explore/#mapview" class="btn btn-inverse"><i class="icon icon-globe"></i> <?php _e('Map View','glp'); ?></a>
+	</div>
 </div>
 
 <div id="stage"></div>
-
-<div id="explore">
-	<h4><?php _e('Explore the Collection','glp'); ?></h4>
-	<a href="/explore/#gridview" class="btn"><i class="icon icon-white icon-th-large"></i> <?php _e('Grid View','glp'); ?></a>
-	<a href="/explore/#mapview" class="btn"><i class="icon icon-white icon-globe"></i> <?php _e('Map View','glp'); ?></a>
-</div>
