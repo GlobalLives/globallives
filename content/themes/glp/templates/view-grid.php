c85b34f375cf39d4e506dcf9008a911a6c19d651
Stanley Jones <stanley@diligentcreative.com>
2013-05-28 11:48:15 -0700

    Explore grid is 4x4 with intelligent scaling for responsive.

--- content/themes/glp/templates/view-grid.php
+++ content/themes/glp/templates/view-grid.php
@@ -1,5 +1,5 @@
 <?php global $participants; ?>
-<div id="gridview" class="view">
+<div id="gridview" class="view"><div class="container">
 	<?php foreach ($participants as $participant) : ?>
 		<article id="participant-<?php echo $participant->ID; ?>" class="participant-grid<?php echo get_field('proposed',$participant->ID) ? ' hide' : ''; ?>"><a href="<?php echo get_permalink($participant->ID); ?>">
 			<div class="participant-meta">
@@ -9,4 +9,4 @@
 			<img src="<?php the_participant_thumbnail_url( $participant->ID, 'medium' ); ?>">
 		</a></article>
 	<?php endforeach; ?>
-</div>
+</div></div>
