131382fa3c7a9561ee238441d569ae26dba8e3b7
Stanley Jones <stanley@diligentcreative.com>
2014-02-20 22:14:06 -0800

    Added occupation, themes to grid view. Closes #367.

--- wp-content/themes/glp/templates/view-grid.php
+++ wp-content/themes/glp/templates/view-grid.php
@@ -1,10 +1,20 @@
-<?php global $participants; ?>
+<?php global $participants, $field_keys; ?>
 <div id="gridview" class="view"><div class="container">
 	<?php foreach ($participants as $participant) : ?>
 		<article id="participant-<?php echo $participant->ID; ?>" class="participant-grid<?php echo get_field('proposed',$participant->ID) ? ' proposed hide' : ''; ?>"><a href="<?php echo get_permalink($participant->ID); ?>">
 			<div class="participant-meta">
 				<h3><?php echo $participant->post_title; ?></h3>
-				<p><?php the_field('location',$participant->ID); ?></p>
+				<p>
+					<?php the_field($field_keys['participant_occupation'],$participant->ID); ?>
+					<?php _e('in','glp'); ?>
+					<?php the_field($field_keys['participant_location'],$participant->ID); ?>
+				</p>
+				<p>
+					<?php if ($themes = get_the_terms($participant->ID,'themes')) : ?>
+					<b><?php _e('Themes: ','glp'); ?></b>
+					<?php foreach ($themes as $theme) { echo $theme->name . ' '; } ?>
+					<? endif; ?>
+				</p>
 			</div>
 			<img src="<?php the_participant_thumbnail_url( $participant->ID, 'medium' ); ?>">
 		</a></article>
