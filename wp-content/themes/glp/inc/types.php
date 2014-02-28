d0b7328e8bf3f094fda7f0db2a38c0fad2069705
Stanley Jones <stanley@diligentcreative.com>
2014-02-27 23:30:17 -0800

    Switched to field_keys for profile submit.

--- wp-content/themes/glp/inc/types.php
+++ wp-content/themes/glp/inc/types.php
@@ -226,20 +226,21 @@
 	}
 
 	function is_profile_created( $user_id ) {
+		global $field_keys;
 		$user = get_userdata( $user_id );
 		if ( // Check all required fields
 			$user->user_firstname &&
 			$user->user_lastname &&
-			get_field('user_occupation','user_'.$user_id) &&
-			get_field('user_location','user_'.$user_id)
+			get_field($field_keys['user_occupation'], 'user_'.$user_id) &&
+			get_field($field_keys['user_location'], 'user_'.$user_id)
 		) {
 			return true;
 		} else {
 			echo "<!-- USER (" . $user_id . ")";
 			echo "\nFirst Name: " . $user->user_firstname;
 			echo "\nLast Name: " . $user->user_lastname;
-			echo "\nOccupation: " . get_field('user_occupation','user_'.$user_id);
-			echo "\nLocation: " . get_field('user_location','user_'.$user_id);
+			echo "\nOccupation: " . get_field($field_keys['user_occupation'], 'user_'.$user_id);
+			echo "\nLocation: " . get_field($field_keys['user_location'], 'user_'.$user_id);
 			// print_r($user);
 			echo "\n\n -->";
 			return false;
