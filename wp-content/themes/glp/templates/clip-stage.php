aa8f3ff7b027d26a5dfb4e6c42a5631e972183e3
Stanley Jones <stanley@diligentcreative.com>
2014-03-10 16:51:36 -0700

    Reset box-sizing on embed input. And z-index fixes. Closes #394.

--- wp-content/themes/glp/templates/clip-stage.php
+++ wp-content/themes/glp/templates/clip-stage.php
@@ -11,15 +11,14 @@
 		<?php $item_id = get_the_ID(); $class = "btn"; include(locate_template('templates/link-queue.php')); ?>
 		<?php if ($download_url = get_field('download_url')) : ?><a href="<?php echo $download_url; ?>" class="btn"><i class="icon icon-white icon-arrow-down"></i> Download</a><?php endif; ?>
 	</div>
-	
-	<div class="modal hide" id="embed-<?php the_field('youtube_id'); ?>">
-		<div class="modal-header"><?php _e('Embed code','glp'); ?></div>
-		<div class="modal-body">
-			<input class="copyable" value="&lt;iframe width=&quot;560&quot; height=&quot;315&quot; src=&quot;http://www.youtube.com/embed/<?php the_field('youtube_id'); ?>&quot; frameborder=&quot;0&quot; allowfullscreen&gt;&lt;/iframe&gt;">
-		</div>
-		<div class="modal-footer">
-			<button class="btn" data-dismiss="modal" aria-hidden="true"><?php _e('Close','glp'); ?></button>
-		</div>
-	</div>
 </article>
+<div class="modal hide" id="embed-<?php the_field('youtube_id'); ?>">
+	<div class="modal-header"><?php _e('Embed code','glp'); ?></div>
+	<div class="modal-body">
+		<input class="copyable" value="&lt;iframe width=&quot;560&quot; height=&quot;315&quot; src=&quot;http://www.youtube.com/embed/<?php the_field('youtube_id'); ?>&quot; frameborder=&quot;0&quot; allowfullscreen&gt;&lt;/iframe&gt;">
+	</div>
+	<div class="modal-footer">
+		<button class="btn" data-dismiss="modal" aria-hidden="true"><?php _e('Close','glp'); ?></button>
+	</div>
+</div>
 <?php endwhile; ?>
