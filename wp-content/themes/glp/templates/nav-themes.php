76ae855de162dd68ef832cb121195b4f8d67f8ee
Stanley Jones <stanley@diligentcreative.com>
2014-04-03 14:46:23 -0700

    Theme carousel working on Explore page. Fixes #446.

--- wp-content/themes/glp/templates/nav-themes.php
+++ wp-content/themes/glp/templates/nav-themes.php
@@ -3,23 +3,28 @@
 	<div class="nav-themes-inner container">
 		<div id="theme-carousel" class="carousel slide">
 			<ul class="carousel-inner">
-				<div class="item active row">
-					<li class="theme-navitem span2 active"><?php echo _e('All Themes','glp'); ?></li>
-					<?php foreach ($themes as $theme) : ?>
+				<li class="theme-navitem span2 active"><?php echo _e('All Themes','glp'); ?></li>			
+<?php
+	$total_themes = count($themes);
+	$themes_per_row = 4;
+	foreach($themes as $i => $theme) :
+?>
+				<?php if ($i % $themes_per_row == 0) : ?><div class="item row<?php if ($i == 0) :?> active<?php endif; ?>"><?php endif; ?>
+				<li class="theme-navitem span2" data-term="<?php echo $theme->slug; ?>">
+					<a class="theme-link hide" href="<?php echo get_term_link($theme,'theme'); ?>">
+						<div class="theme-watch"><?php _e('Watch this theme','glp'); ?></div>
+						<div class="thumbnails">
+						<?php if ($theme_participants = get_posts(array( 'post_type' => 'participant', 'tax_query' => array(array('taxonomy' => 'themes', 'field' => 'slug', 'terms' => $theme->slug)) ))) : foreach ($theme_participants as $theme_participant) : ?>
+							<img src="<?php the_participant_thumbnail_url( $theme_participant->ID, 'small' ); ?>">
+						<?php endforeach; endif; ?>
+						</div>
+					</a>
+					<a class="theme-filter"><?php echo $theme->name; ?></a>
+				</li>
+				<?php if ($i == $total_themes - 1 || $i % $themes_per_row == ($themes_per_row - 1)) : ?></div><?php endif; ?>
 
-					<li class="theme-navitem span2" data-term="<?php echo $theme->slug; ?>">
-						<a class="theme-link hide" href="<?php echo get_term_link($theme,'theme'); ?>">
-							<div class="theme-watch"><?php _e('Watch this theme','glp'); ?></div>
-							<div class="thumbnails">
-							<?php if ($theme_participants = get_posts(array( 'post_type' => 'participant', 'tax_query' => array(array('taxonomy' => 'themes', 'field' => 'slug', 'terms' => $theme->slug)) ))) : foreach ($theme_participants as $theme_participant) : ?>
-									<img src="<?php the_participant_thumbnail_url( $theme_participant->ID, 'small' ); ?>">
-							<?php endforeach; endif; ?>
-							</div>
-						</a>
-						<a class="theme-filter"><?php echo $theme->name; ?></a>
-					</li>
-					<?php endforeach; ?>
-				</div>
+<?php endforeach; ?>
+
 			</ul>
 			<!-- <a class="carousel-control left" href="#theme-carousel" data-slide="prev">&#9664;</a> -->
 			<a class="carousel-control right" href="#theme-carousel" data-slide="next">&#9654;</a>
