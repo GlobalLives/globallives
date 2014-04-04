3e3ab5c7309415c610358184d18c4429ea2bd01b
Stanley Jones <stanley@diligentcreative.com>
2014-04-03 21:26:35 -0700

    Added addthis:url attributes for individual posts in blog index. Ref #485.

--- wp-content/themes/glp/templates/content-post.php
+++ wp-content/themes/glp/templates/content-post.php
@@ -11,7 +11,7 @@
 	    <footer>
 	    	<div class="entry-share">
 	    		<div class="comment-counter"><a href="<?php the_permalink(); ?>"><i class="icon icon-white icon-comment"></i> <?php echo get_comments_number(); ?></a></div>
-	    		<div class="addthis_toolbox">
+	    		<div class="addthis_toolbox" addthis:title="<?php the_title(); ?>" addthis:url="<?php the_permalink(); ?>">
 					<a class="addthis_counter_twitter"></a>
 					<a class="addthis_counter_facebook"></a>
 				</div>
