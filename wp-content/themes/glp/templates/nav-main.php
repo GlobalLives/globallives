762af82e54d1cf8837799f15f07d4bd1e5edf3cd
Isabelle Setti <isabellesetti@gmail.com>
2015-05-03 11:05:04 -0700

    Bug fix in glp theme: consistent class name for header nav
    Make sure the "Primary Header Navigation" menu is always rendered with the same
    class name "menu-primary-header-navigation-container" so that the correct style
    gets applied.

--- wp-content/themes/glp/templates/nav-main.php
+++ wp-content/themes/glp/templates/nav-main.php
@@ -1,6 +1,6 @@
 <nav id="nav-main" role="navigation">
 	<div id="nav-main-inner" class="container">
-	<?php if (has_nav_menu('primary_header_navigation')) { wp_nav_menu(array('theme_location' => 'primary_header_navigation')); } ?>
+	<?php if (has_nav_menu('primary_header_navigation')) { wp_nav_menu(array('theme_location' => 'primary_header_navigation', 'container_class' => 'menu-primary-header-navigation-container')); } ?>
 	<?php if (has_nav_menu('social_navigation')) { wp_nav_menu(array('theme_location' => 'social_navigation')); } ?>
 	</div><!-- /#nav-main-inner -->
 </nav>
