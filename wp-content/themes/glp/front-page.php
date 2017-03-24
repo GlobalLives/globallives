5aa019f3b8ca4237b460b577513d9deca0ccff2d
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-03-23 21:59:08 -0700

    edited the GLP Thumbnails

--- wp-content/themes/glp/front-page.php
+++ wp-content/themes/glp/front-page.php
@@ -12,31 +12,47 @@
 				<h2><?php echo $participant->post_title; ?> &mdash; <span class="participant-location"><?php the_field($field_keys['participant_location'], $participant->ID); ?></span></h2>
 				
 				<div class="participant-meta row">
-					<div class="span6 pull-left tinyDetails">
+					<div class="span4 pull-left tinyDetails">
+
+						<img src="http://maps.googleapis.com/maps/api/staticmap?center=<?php the_field($field_keys['participant_latitude'],$participant->ID); ?>,<?php the_field($field_keys['participant_longitude'],$participant->ID); ?>&zoom=6&size=600x400&markers=color:red|<?php the_field($field_keys['participant_latitude'],$participant->ID); ?>,<?php the_field($field_keys['participant_longitude'],$participant->ID); ?>&maptype=roadmap&sensor=false&style=feature:all%7Celement:geometry%7Csaturation:-100" class="location" />
+
 						<div class="row">
-							<div class="span3">
+							<div class="span2">
 								<b><?php _e('Occupation','glp'); ?>:</b> <?php the_field($field_keys['participant_occupation'], $participant->ID); ?><br>
-								<?php if ($dob = get_field($field_keys['participant_dob'], $participant->ID)) : ?><b><?php _e('Date of Birth','glp'); ?>:</b> <?php echo $dob; ?><?php endif; ?>
+								<?php if (
+								$dob = get_field($field_keys['participant_dob'], $participant->ID)) : ?><b><?php _e('Date of Birth','glp'); ?>:</b> <?php echo $dob; ?><?php endif; ?>
+								<br>
+								<br>
 							</div>
-							<div class="span3">
+							<div class="span2">
 								<b><?php _e('Religion','glp'); ?>:</b> <?php the_field($field_keys['participant_religion'], $participant->ID); ?><br>
 								<b><?php _e('Income','glp'); ?>:</b> <?php 
 								$incomes = get_field_object($field_keys['participant_income']); $income = get_field($field_keys['participant_income'], $participant->ID); echo $incomes['choices'][$income]; ?>
 							</div>
-							<div class="span6">
+							<div class="span4">
 								<p><?php echo $participant->post_content; ?></p>
-								<a href="<?php echo get_permalink($participant->ID); ?>" class="pull-right btn"><i class="icon-white icon-play"></i> Learn More</a>
+								<a href="<?php echo get_permalink($participant->ID); ?>" class="btn btn-inverse"><i class="icon icon-play"></i> Full Story</a>
 							</div>
 						</div>
 					</div><!-- .tinyDetails -->
-					<img src="http://maps.googleapis.com/maps/api/staticmap?center=<?php the_field($field_keys['participant_latitude'],$participant->ID); ?>,<?php the_field($field_keys['participant_longitude'],$participant->ID); ?>&zoom=6&size=570x250&markers=color:red|<?php the_field($field_keys['participant_latitude'],$participant->ID); ?>,<?php the_field($field_keys['participant_longitude'],$participant->ID); ?>&maptype=roadmap&sensor=false&style=feature:all%7Celement:geometry%7Csaturation:-100" class="location" />
+
+					<div class="span8">
+					<?php
+						if ( $summary_video = get_field($field_keys['participant_summary_video'],$participant->ID) ) {
+							query_posts(array( 'post_type' => 'clip', 'p' => $summary_video[0]->ID ));
+							get_template_part('templates/clip','summary');
+							wp_reset_query();
+						}
+					?>
+					</div>
 				</div><!-- .participant-meta -->
 			</div>
 		</article>
+
 	<?php endforeach; ?>
 	<div id="explore">
 		<h4><?php _e('Explore the Collection','glp'); ?></h4>
-		<a href="/explore/#gridview" class="btn"><i class="icon-white icon-th-large"></i> <?php _e('Grid View','glp'); ?></a>
-		<a href="/explore/#mapview" class="btn"><i class="icon-white icon-globe"></i> <?php _e('Map View','glp'); ?></a>
+		<a href="/explore/#gridview" class="btn btn-inverse"><i class="icon icon-th-large"></i> <?php _e('Grid View','glp'); ?></a>
+		<a href="/explore/#mapview" class="btn btn-inverse"><i class="icon icon-globe"></i> <?php _e('Map View','glp'); ?></a>
 	</div>
 </div>
