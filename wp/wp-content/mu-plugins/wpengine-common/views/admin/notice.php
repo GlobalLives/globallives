4d1c9382189da3b778db567405764ca2073f48b6
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-03-25 13:59:21 -0700

    removed WP directory: no reason to exist

--- wp/wp-content/mu-plugins/wpengine-common/views/admin/notice.php
+++
@@ -1,6 +0,0 @@
-<div class="wpe-notices updated wpe-notices-<?php echo $class; ?>" title="<?php echo $id; ?>">
-	<div class="dismissable"><img src="<?php echo $icon; ?>" id="dismiss-it" alt="<?php _e('Dismiss this message','wpengine'); ?>"></div>
-	<p class="wpe-notices-<?php echo $class; ?>">
-		<strong><?php echo ucwords( $class ); ?>: </strong><?php _e($message,'wpengine'); ?>
-	</p>
-</div>
--- wp/wp-content/mu-plugins/wpengine-common/views/general/powered-by.php
+++
@@ -1,1 +0,0 @@
-Powered by <a href="<?php echo esc_url( $affiliate_link ? $affiliate_link : 'https://wpengine.com' ); ?>" title="<?php esc_attr_e( 'Managed WordPress Hosting', 'wpengine' ); ?>" target="_blank"><img style="width: 100px;" src="<?php echo esc_url( $logo ); ?>"></a>
