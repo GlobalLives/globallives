c79e23d4a1f4ec3afda813d3bc389502ea767135
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-03-25 23:59:02 -0700

    home thumbnail data-controls attribute added

--- wp-content/themes/glp/templates/nav-featured.php
+++ wp-content/themes/glp/templates/nav-featured.php
@@ -9,7 +9,7 @@
 
 				<div class="item active row">
 					<ul>
-						<li class="featured-thumbnail home-thumbnail span2 active">
+						<li class="featured-thumbnail home-thumbnail span2 active" data-controls="content_glp">
 							<a href="#content_glp" aria-controls="content_glp" role="tab" data-toggle="tab" title="Global Lives Matter">
 							<img src="<?php bloginfo('stylesheet_directory'); ?>/img/logo-featured.png">
 						</li>
