84760f7a910f8cc3a438c8619e9c9398f32f24a0
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-04-20 11:02:48 -0700

    identifying used scripts and stylesheets to minimize enqueue

--- wp-content/themes/glp/inc/queue.php
+++ wp-content/themes/glp/inc/queue.php
@@ -11,48 +11,6 @@ function glp_queue() {
 		wp_register_script('jquery', null, null, '2.0.3', false);
 	}
 
-	if (WP_ENV == 'DEV') { // Local development environment, load scripts separately.
-
-		// Stylesheets
-		wp_enqueue_style('glp_style', get_template_directory_uri() . '/css/style.min.css', false, null);
-		wp_enqueue_style('jquery-ui-custom', get_template_directory_uri() . '/css/jquery-ui/jquery-ui-1.9.2.custom.min.css');
-
-		// Register scripts: wp_register_script( $handle, $src, $deps, $ver, $in_footer )
-
-		wp_register_script('glp_main', get_template_directory_uri() . '/js/main.js', array('jquery','jquery-cycle'), null, true);
-		wp_register_script('glp_video', get_template_directory_uri() . '/js/video.js', array('jquery','jquery-ui-slider', 'jquery-ui-touch'), null, true);
-
-		wp_register_script('d3', get_template_directory_uri() . '/js/vendor/d3.v3.min.js', false, null, true);
-		wp_register_script('bootstrap', get_template_directory_uri() . '/js/vendor/bootstrap-2.3.2.min.js', 'jquery', null, true);
-		wp_register_script('jquery-cycle', get_template_directory_uri() . '/js/vendor/jquery.cycle.lite.js', 'jquery','1.7', true);
-		wp_register_script('jquery-ui-slider', false, false, false, true);
-		wp_register_script('jquery-ui-touch', get_template_directory_uri() . '/js/vendor/jquery.ui.touch-punch.min.js', array('jquery','jquery-ui-slider'), false, true);
-		wp_register_script('addthis', '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-510832576c1fd9d6', false, null, true);
-
-		// Enqueue scripts for ALL pages
-		wp_enqueue_script('glp_main');
-	    wp_enqueue_script('glp_video');
-		wp_enqueue_script('bootstrap');
-		wp_enqueue_script('addthis');
-	
-		wp_enqueue_script('jquery-ui-slider');
-		wp_enqueue_script('jquery-ui-touch');
-
-		// Enqueue scripts for "Single" pages
-		if (is_single() && comments_open() && get_option('thread_comments')) {
-			wp_enqueue_script('comment-reply');
-		}
-	
-		// Enqueue scripts for "Explore", "Series", and "Pariticpant" pages
-		if (is_page('explore') || is_tax('series') || is_singular('participant')) {
-			wp_enqueue_script('d3');
-			wp_enqueue_script('jquery-cycle');
-		}
-
-		// Localize glpAjax.ajaxurl
-		wp_localize_script('glp_main', 'glpAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
-
-	} else { // Production environment, load concatented scripts.
 
 		// Stylesheets
 		wp_enqueue_style('glp_style', get_template_directory_uri() . '/css/style.min.css', false, filemtime(get_stylesheet_directory() . '/css/style.min.css'));
@@ -84,8 +42,6 @@ function glp_queue() {
 
 		// Localize glpAjax.ajaxurl
 		wp_localize_script('glp_app', 'glpAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' )));
-
-	}
 
 }
 
