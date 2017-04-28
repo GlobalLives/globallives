2b83252a5a6e67cf3f6770c707762746365926bf
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-04-27 19:39:51 -0700

    Changed the app.js

--- wp-content/themes/glp/js/app.js
+++ wp-content/themes/glp/js/app.js
@@ -1,16 +1,11 @@
 $(function() {
     $("#nav-featured .participant-thumbnail").click(function(){
+        $(".participant-thumbnaill").removeClass("active")
         var a = $(this).attr("data-controls");
-        var b = a.replace('content_','#');
-        var c = a.replace('content_','#location_');
-        var d = a.replace('content_','#video_');
-        var e = $(c).attr("alt")
-        e = e.replace(" ","");
-        $.ajax({
-            success: function( data ) {
-                
-            }
-        })
-        $(c).attr({'src':"http://maps.googleapis.com/maps/api/staticmap?center=" + e + "&zoom=6&size=600x400&markers=color:red|" + e + "&maptype=roadmap&sensor=false&style=feature:all%7Celement:geometry%7Csaturation:-100"})
+        var c = a.replace("content_","#location_"); // create the name of the map image from the data-controls field
+        var e = $(c).attr("alt") // get the lat/long of the individual
+        e = e.replace(" ",""); // replace the space from the alt tag with nothing so it can be put in an URL
+        //---- add the src of the location image
+        $(c).attr({"src":"http://maps.googleapis.com/maps/api/staticmap?center=" + e + "&zoom=6&size=600x400&markers=color:red|" + e + "&maptype=roadmap&sensor=false&style=feature:all%7Celement:geometry%7Csaturation:-100"})
     })
 })
