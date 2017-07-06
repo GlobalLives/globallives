e93d98fe45d034bed6620dfbaa30ea9c0b8c6065
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-07-05 21:56:23 -0700

    Council Page Template

---
+++ wp-content/themes/glp/page-council.php
@@ -0,0 +1,28 @@
+<h1 class="section-title profile-title"><?php echo __('About','glp'); ?></h1>
+<div class="page-container static-page-container container">
+  <div class="row">
+    <div class="span3">
+      <?php get_template_part('templates/nav','local'); ?>
+    </div>
+    <div class="span9">
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header>
+          <h2 class="page-title"><?php the_title(); ?></h2>
+          <?php get_template_part('templates/entry-meta'); ?>
+        </header>
+        <div class="entry-summary">
+          <?php $rows = get_field('individual'); //sets the feildname of ACF ?>
+          <?php foreach ($rows as $individual) { ?>
+            <div>
+              <h4><?php echo $individual['individual_name']; //getting the repeater information ?> <small><?php echo $individual['individual_title']; //getting the repeater information ?></small></h4>
+              <?php if($individual['individual_picture']) { //checks for image presence in the repeater information ?><img src="<?php echo $individual['individual_picture']; ?>" /><?php } ?><?php echo $individual['individual_description']; //getting the repeater information ?>
+            </div>
+          <?php } ?>
+        </div>
+        <footer>
+          <?php the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
+        </footer>
+      </article>
+    </div><!-- .span9 -->
+  </div><!-- .row -->
+</div><!-- .container -->
