7ebd2b1040ee401c0374c04b92cc247a3d20b52a
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-08-21 15:28:13 -0700

    [PLUGIN UPDATE] The Events Calendar

--- wp-content/themes/glp/templates/header.php
+++ wp-content/themes/glp/templates/header.php
@@ -15,8 +15,8 @@
 	</div>
 	<div id="masthead" class="container">
 		<div class="row">
-			<h1 class="site-title span6"><a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
-			<div class="site-tabs span6">
+			<h1 class="site-title col-sm-6"><a class="brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></h1>
+			<div class="site-tabs col-sm-6">
 				<div class="tab-pane active" id="searchtab"><?php get_template_part('templates/form','search'); ?></div>
 			</div>
 		</div>
