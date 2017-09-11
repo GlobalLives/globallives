e79101ebc2a55e88c036f2dc3bdbe87e11f8457c
Benjamin Meyers <benjamin@flowerjoiner.com>
2017-09-11 11:03:03 -0700

    ignoring plugind

--- wp-content/plugins/googleanalytics/view/ga_auth_button.php
+++
@@ -1,7 +0,0 @@
-<button id="ga_authorize_with_google_button" class="<?php echo ( $type == 'auth' ) ? 'button-primary' : 'button-secondary' ?>"
-	<?php if ( Ga_Helper::are_features_enabled() ) : ?>
-		onclick="ga_popup.authorize( event, '<?php echo esc_attr( $url ); ?>' )"
-	<?php endif; ?>
-	<?php echo( ( esc_attr( $manually_id ) || ! Ga_Helper::are_features_enabled()  || Ga_Helper::is_curl_disabled() ) ? 'disabled="disabled"' : '' ); ?>
-><?php _e( $label ) ?>
-</button>
--- wp-content/plugins/googleanalytics/view/statistics.php
+++
@@ -1,6 +0,0 @@
-<div class="wrap ga-wrap">
-    <h2>Google Analytics - <?php _e( 'Dashboard' ); ?></h2>
-    <div class="ga_container" id="exTab2">
-		<?php echo $data ?>
-    </div>
-</div>
