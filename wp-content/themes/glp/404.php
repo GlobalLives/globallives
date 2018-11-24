9e0f2c6a38e832c2938f615c804377a70e2fbfa5
Benjamin Meyers <benjamin@flowerjoiner.com>
2018-11-24 11:26:36 -0800

    #638 changed the 404 grammer

--- wp-content/themes/glp/404.php
+++ wp-content/themes/glp/404.php
@@ -1,9 +1,9 @@
 <article id="post-0" class="post error404 no-results not-found">
 	<header class="entry-header">
-		<h1 class="entry-title"><?php _e( 'Missing!', 'glp' ); ?></h1>
+		<h1 class="entry-title"><?php _e( 'Missing Content!', 'glp' ); ?></h1>
 	</header>
 	<div class="entry-content">
-		<p><?php _e( 'We can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help?', 'glp' ); ?></p>
+		<p><?php _e( 'Our life experience isn&rsquo;t complete with what you&rsquo;re looking for. Please be empathetic to our project and continue your experience by searching:', 'glp' ); ?></p>
 		<?php get_search_form(); ?>
 	</div><!-- .entry-content -->
 </article><!-- #post-0 -->
