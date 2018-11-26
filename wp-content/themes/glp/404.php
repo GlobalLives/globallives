6be3a988bf8d411cddcc8b81d2c3c0f0f0c9992d
Benjamin Meyers <benjamin@flowerjoiner.com>
2018-11-25 21:20:54 -0800

    modified 404 page

--- wp-content/themes/glp/404.php
+++ wp-content/themes/glp/404.php
@@ -2,8 +2,11 @@
 	<header class="entry-header">
 		<h1 class="entry-title"><?php _e( 'Missing Content!', 'glp' ); ?></h1>
 	</header>
-	<div class="entry-content">
-		<p><?php _e( 'Our life experience isn&rsquo;t complete with what you&rsquo;re looking for. Please be empathetic to our project and continue your experience by searching:', 'glp' ); ?></p>
-		<?php get_search_form(); ?>
+	<div class="entry-content" style="position:relative;">
+    <div style="font-size:120px;color:#eee;position:absolute;top:60px;right:40px;z-index:10">404</div>
+		<p style="position:relative;z-index:15"><?php _e( 'Our life experience isn&rsquo;t complete with what you&rsquo;re looking for. Please be empathetic to our project and continue your experience by searching:', 'glp' ); ?></p>
+		<div style="position:relative;z-index:15">
+      <?php get_search_form(); ?>
+    </div>
 	</div><!-- .entry-content -->
 </article><!-- #post-0 -->
