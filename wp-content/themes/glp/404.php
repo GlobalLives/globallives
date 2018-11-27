4f584441dd37709a144e21308243684bce9d42bc
Benjamin Meyers <benjamin@flowerjoiner.com>
2018-11-27 10:40:58 -0800

    Changed color of 404 indicator

--- wp-content/themes/glp/404.php
+++ wp-content/themes/glp/404.php
@@ -3,7 +3,7 @@
 		<h1 class="entry-title"><?php _e( 'Missing Content!', 'glp' ); ?></h1>
 	</header>
 	<div class="entry-content" style="position:relative;">
-    <div style="font-size:120px;color:#eee;position:absolute;top:60px;right:40px;z-index:10">404</div>
+    <div style="font-size:120px;color:#f9e0c5;position:absolute;top:60px;right:40px;z-index:10">404</div>
 		<p style="position:relative;z-index:15"><?php _e( 'Our life experience isn&rsquo;t complete with what you&rsquo;re looking for. Please be empathetic to our project and continue your experience by searching:', 'glp' ); ?></p>
 		<div style="position:relative;z-index:15">
       <?php get_search_form(); ?>
