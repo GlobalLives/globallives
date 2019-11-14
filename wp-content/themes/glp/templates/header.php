29f09dc33071d65542ac3fc4f1dedbfd6a607005
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-11-13 16:44:48 -0800

    #648 CSS hamburger

--- wp-content/themes/glp/templates/header.php
+++ wp-content/themes/glp/templates/header.php
@@ -23,8 +23,11 @@
 	</div>
 	<nav id="nav-main" role="navigation">
 		<div id="nav-main-inner" class="container">
-		<?php if (has_nav_menu('primary_header_navigation')) { wp_nav_menu(array('theme_location' => 'primary_header_navigation')); } ?>
-		<?php if (has_nav_menu('social_navigation')) { wp_nav_menu(array('theme_location' => 'social_navigation')); } ?>
+      <!-- [THE SHRINKER] -->
+      <label for="shrinker">&#9776;</label>
+      <input type="checkbox" id="shrinker"/>
+			<?php if (has_nav_menu('primary_header_navigation')) { wp_nav_menu(array('theme_location' => 'primary_header_navigation')); } ?>
+			<?php if (has_nav_menu('social_navigation')) { wp_nav_menu(array('theme_location' => 'social_navigation')); } ?>
 		</div><!-- /#nav-main-inner -->
 	</nav>
 	<?php if (is_front_page()) : ?>
