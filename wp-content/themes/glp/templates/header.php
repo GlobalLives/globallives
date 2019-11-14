d4529d259b533f6bfe30a4a258f555df641c7059
Benjamin Meyers <benjamin@purespiritwebworks.com>
2019-11-13 23:33:19 -0800

    #649 removed borders and improved instructions

--- wp-content/themes/glp/templates/header.php
+++ wp-content/themes/glp/templates/header.php
@@ -24,8 +24,9 @@
 	<nav id="nav-main" role="navigation">
 		<div id="nav-main-inner" class="container">
       <!-- [THE SHRINKER] -->
-      <label for="shrinker">&#9776;</label>
-      <input type="checkbox" id="shrinker"/>
+      <input type="checkbox" id="shrinker" />
+      <label class="shrinker-icon" for="shrinker">&#9776;</label>
+      <label for="shrinker" class="directions">&larr; click to toggle menu</label>
 			<?php if (has_nav_menu('primary_header_navigation')) { wp_nav_menu(array('theme_location' => 'primary_header_navigation')); } ?>
 			<?php if (has_nav_menu('social_navigation')) { wp_nav_menu(array('theme_location' => 'social_navigation')); } ?>
 		</div><!-- /#nav-main-inner -->
